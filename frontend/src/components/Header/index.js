ed1d818e0a3b84680ab88fafec7155c3b005c2f0
Douglas de Assis da Silva <ddadsx@gmail.com>
2020-11-28 02:19:58 -0400

    chore: Change app name

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Logo, HeaderContainer } from './styles';
-import Icone from '../../assets/check.png'
+import Icone from '../../assets/weee.png'
 
 function Header(props) {
     return (
         <>
             <HeaderContainer>
-                <Logo src={Icone} alt='Pitu - Encurtador de URL'/>
-                <h1>Pitu</h1>
+                <Logo src={Icone} alt='Weee - Encurtador de URL'/>
+                <h1>Weee</h1>
                 <p>{props.children}</p>
             </HeaderContainer>
         </>
